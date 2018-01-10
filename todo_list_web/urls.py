104514df54af2aa73a097c77f87aaa3296f857c4
Felipe Menelau <felipe.menelau@inlocomedia.com>
2018-01-10 09:35:15 -0300

    feat: adds basic jwt auth

--- todo_list_web/urls.py
+++ todo_list_web/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/token/$', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    url(r'^api/token/refresh/$', TokenRefreshView.as_view(), name='token_refresh'),
 ]
