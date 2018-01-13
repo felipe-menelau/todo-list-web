de824158a1a66a205607aea402fffcd203ea4bea
Felipe Menelau <felipe.menelau@inlocomedia.com>
2018-01-13 11:34:51 -0300

    chore: adds email configuration

--- todo_list_web/settings.py
+++ todo_list_web/settings.py
@@ -131,3 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Email Config
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
