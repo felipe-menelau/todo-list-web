39781158df27bfd75b74f3e4c85249f061706546
Felipe Menelau <felipe.menelau@inlocomedia.com>
2018-01-13 12:30:49 -0300

    chore: adds django-extesions to settings

--- todo_list_web/settings.py
+++ todo_list_web/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api.apps.ApiConfig'
+    'api.apps.ApiConfig',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
