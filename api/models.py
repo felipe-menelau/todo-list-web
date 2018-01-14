8174ee288eaf7b3f0e795a17bc35018fdd83890e
Felipe Menelau <felipe.menelau@inlocomedia.com>
2018-01-13 21:33:22 -0300

    feat: adds new model for todolists

--- api/models.py
+++ api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TODOList(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    title = models.CharField(max_length=100)
+
