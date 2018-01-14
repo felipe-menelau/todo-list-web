35b3217416a9c3f3c3d31a928fc0a10a44320279
Felipe Menelau <felipe.menelau@inlocomedia.com>
2018-01-13 23:59:08 -0300

    test: add new test case for creating new task

--- api/tests.py
+++ api/tests.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from api.tokens import account_activation_token
+from datetime import datetime
 
 class UserTest(APITestCase):
     def setUp(self):
@@ -121,3 +122,25 @@ class TODOListTests(APITestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(todo_to_edit.title, data['title'])
+
+class TaskTest(APITestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user('testuser', 'test@example.com', 'testpassword')
+
+        self.test_todo_list = self.test_user.todolist_set.create(title='Teste')
+
+        self.client.force_authenticate(user=self.test_user)
+
+    def test_create_task(self):
+        data = {
+            'title': 'Do todolist project',
+            'deadline': datetime.strptime('16-01-2017', '%d-%m-%Y')
+        }
+
+        response = self.client.post(f'/api/todo/{self.test_todo_list.id}/tasks/', data, format='json')
+
+        self.test_todo_list.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(self.test_todo_list.task_set.first().title, data['title'])
+        self.assertEqual(self.test_todo_list.task_set.first().deadline.replace(tzinfo=None), data['deadline'])
