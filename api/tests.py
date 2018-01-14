4836ae34909d76d1af4b43ceee20ff3fe6951e7f
Felipe Menelau <felipe.menelau@inlocomedia.com>
2018-01-13 22:29:37 -0300

    test: adds tests for todolist model

--- api/tests.py
+++ api/tests.py
@@ -81,3 +81,29 @@ class UserTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
         self.assertEqual(reseting_user.check_password(new_password), True)
 
+class TODOListTests(APITestCase):
+    def setUp(self):
+        self.test_user = User.objects.create_user('testuser', 'test@example.com', 'testpassword')
+
+        self.todo_create_url = '/api/todo/'
+
+        self.client.force_authenticate(user=self.test_user)
+
+    def test_create_todo_list(self):
+        data = {
+            'title': 'List',
+        }
+
+        response = self.client.post(self.todo_create_url, data, format='json')
+
+        self.test_user.refresh_from_db()
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(self.test_user.todolist_set.first().title, data['title'])
+
+    def test_delete_todo_list(self):
+        todo_to_delete = self.test_user.todolist_set.create(title='Teste')
+
+        response = self.client.delete(f'/api/todo/{todo_to_delete.id}/', format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
