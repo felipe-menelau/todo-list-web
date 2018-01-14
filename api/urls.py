a9b58beb24daf65ad7cc9784c959ec73032aa954
Felipe Menelau <felipe.menelau@inlocomedia.com>
2018-01-14 00:00:08 -0300

    feat: adds urls for dealing with tasks

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'api/users$', views.UserCreate.as_view(), name='user-create'),
     url(r'api/users/activate$', views.UserActivate.as_view(), name='user-activate'),
     url(r'api/users/forgot$', views.UserPasswordManagement.as_view(), name='user-forgot'),
+    url(r'api/todo/(?P<pk>[0-9]+)/tasks/', views.TaskCreation.as_view(), name='tasks'),
+    url(r'api/todo/(?P<pk>[0-9]+)/tasks/(?P<pk_task>[0-9]+)', views.TaskManagement.as_view(), name='task-detail'),
 ]
