609d74a9f05e306c6fcdde854cd86fba1b279932
Felipe Menelau <felipe.menelau@inlocomedia.com>
2018-01-14 12:58:00 -0300

    feat: adds the pdf download route

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import task_views, todo_views, user_views
+from .views import task_views, todo_views, user_views, pdf_views
 
 urlpatterns = [
     url(r'api/users$', user_views.UserCreate.as_view(), name='user-create'),
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'api/users/forgot$', user_views.UserPasswordManagement.as_view(), name='user-forgot'),
     url(r'api/todo/(?P<pk>[0-9]+)/tasks/$', task_views.TaskCreation.as_view(), name='tasks'),
     url(r'api/todo/(?P<pk>[0-9]+)/tasks/(?P<pk_task>[0-9]+)/$', task_views.TaskManagement.as_view(), name='task-detail'),
+    url(r'api/todo/(?P<pk>[0-9]+)/pdf$', pdf_views.PdfExport.as_view(), name='todo-pdf'),
 ]
