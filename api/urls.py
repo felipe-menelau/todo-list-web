852d5c1c624096ec482b9e93c0cf797a65fa1354
Felipe Menelau <felipe.menelau@inlocomedia.com>
2018-01-14 09:40:39 -0300

    fix: fixes task urls ending incorrectly

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     url(r'api/users$', user_views.UserCreate.as_view(), name='user-create'),
     url(r'api/users/activate$', user_views.UserActivate.as_view(), name='user-activate'),
     url(r'api/users/forgot$', user_views.UserPasswordManagement.as_view(), name='user-forgot'),
-    url(r'api/todo/(?P<pk>[0-9]+)/tasks/', task_views.TaskCreation.as_view(), name='tasks'),
-    url(r'api/todo/(?P<pk>[0-9]+)/tasks/(?P<pk_task>[0-9]+)', task_views.TaskManagement.as_view(), name='task-detail'),
+    url(r'api/todo/(?P<pk>[0-9]+)/tasks/$', task_views.TaskCreation.as_view(), name='tasks'),
+    url(r'api/todo/(?P<pk>[0-9]+)/tasks/(?P<pk_task>[0-9]+)/$', task_views.TaskManagement.as_view(), name='task-detail'),
 ]
