3a76da74ffffec986944d52bba22051bc5da100a
Felipe Menelau <felipe.menelau@inlocomedia.com>
2018-01-13 23:59:36 -0300

    feat: adds task model

--- api/models.py
+++ api/models.py
@@ -7,3 +7,10 @@ class TODOList(models.Model):
     updated_at = models.DateField(auto_now=True)
     title = models.CharField(max_length=100)
 
+class Task(models.Model):
+    owner = models.ForeignKey(TODOList, on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    title = models.CharField(max_length=100)
+    done = models.BooleanField(default=False)
+    deadline = models.DateTimeField()
